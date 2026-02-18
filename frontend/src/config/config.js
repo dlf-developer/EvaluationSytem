b8f83eca6b86daa2a4ed665b3d8775b5984f2483
dlf-developer <Developer@dlps.co.in>
2026-02-19 01:57:04 +0530

    feat: Configure API base URL using environment variables defined in a new .env file.

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,8 +1,12 @@
-module.exports={
-    baseURL:"http://localhost:8001/api/",
+const url = process.env.REACT_APP_API_URL;
+
+
+
+module.exports = {
+    baseURL:url,
     // baseURL:"https://evaluationapi.dlps.co.in/api/",
     // baseURL:"https://dekhooo.com/api/",
     // baseURL:"http://148.135.136.129/api",
     // baseURL:"https://teacher-evaluation-system-t4rh.onrender.com/api",
-    UserRole:["Superadmin","Observer","Teacher"]
+    UserRole: ["Superadmin", "Observer", "Teacher"]
 }
